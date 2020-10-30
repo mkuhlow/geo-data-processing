c793e8c3731c980ed45964bf147869f57657d12c
Marcel Kuhlow <marcel.kuhlow@codefrog.de>
2020-10-30 12:32:59 +0100

    remove logouts

--- processingCoordinates.js
+++ processingCoordinates.js
@@ -2,16 +2,9 @@ const geolib = require('geolib');
 
 exports.processingCoordinates = function(objectPositions, currendPosition, radius) {
     var objectsInRadius = [];
-    console.log('________________________');
-    console.log(Object.keys(objectPositions));
-    const keys = Object.keys(objectPositions);
-    console.log('________________________');
-    console.log(objectPositions);
+
     for (var i = 0; i < objectPositions.cars.length; i++) {
-        console.log(objectPositions.cars[i]);
-        console.log(objectPositions.cars[i].coordinates.lat);
-        console.log(objectPositions.cars[i].coordinates.lon);
-console.log(currendPosition);
+
         var carCoordinate = {
             latitude: objectPositions.cars[i].coordinates.lat,
             longitude: objectPositions.cars[i].coordinates.lon,
@@ -21,9 +14,7 @@ console.log(currendPosition);
 
         if (objectDistance <= radius) {
             objectsInRadius.push({ id: objectPositions.cars[i].id, carDistance: objectDistance });
-            console.log('Das Ziel ist in der erwarteten Entfernugn = ' + objectDistance);
         }
-
     }
 
     objectsInRadius.sort(function(a,b) {
